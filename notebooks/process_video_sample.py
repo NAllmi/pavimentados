5042ad85718f552e6bb82983c2c265f1847d1d8c
Victor Durand <durandv@gmail.com>
2024-08-18 15:51:27 -0300

    update config

--- notebooks/process_video_sample.py
+++ notebooks/process_video_sample.py
@@ -10,8 +10,7 @@ if __name__ == "__main__":
     input_path = Path("./road_videos")
     models_path = Path("../models/artifacts")
 
-    input_video_name = "20230403M-F-P16"
-    # input_video_name = "sample"
+    input_video_name = "sample"
 
     output_path = Path("./outputs") / input_video_name
     output_path.mkdir(parents=True, exist_ok=True)
